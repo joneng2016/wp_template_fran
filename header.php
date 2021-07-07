c3907b040e411daf08a9a241a32510012b4a8f8e
Jonathan Morris Samara <joneng2016@github.com>
2021-07-06 21:52:05 -0300

    menu pronto

--- header.php
+++ header.php
@@ -29,10 +29,62 @@
     
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+        crossorigin="anonymous"
+    >
+
+
 </head>
 
 <body>
 
-    <div id="ttr_header" class="jumbotron">
-        <h2>HEADER</h2>
-    </div>
+<header>
+
+    <nav class="navbar navbar-expand-lg navbar-light roxo-menu">
+        <div class="container-fluid">
+            
+            <button 
+                class="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navbarNav" 
+                aria-controls="navbarNav"
+                 aria-expanded="false" 
+                 aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+
+                    <li class="nav-item">
+                        <a class="menu-link active" aria-current="page" href="#">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="menu-link" href="#">Serviços</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="menu-link" href="#">Fotos</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="menu-link" href="#">Vídeos</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="menu-link" href="#">Como Chegar</a>
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+
+</header>
